58b6f03ba08405e3212fb51d93bc86af37e0095d
Lolotuber <121623389+LoloTuber@users.noreply.github.com>
2025-12-08 21:02:06 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -5,16 +5,15 @@ import os
 def loadfile(path):
   with open(path,"r",encoding="utf-8") as file:
     data = json.load(file)
-  file.close()
   return data
 
 def savefile(path,value):
   with open(path,"w",encoding="utf-8") as file:
     json.dump(value,file)
-  file.close()
 
 settings=loadfile(os.path.dirname(os.path.abspath(__file__))+"/settings.json")
 items = loadfile(os.path.dirname(os.path.abspath(__file__))+"/list.json")
 
 def getrandomelement():
   return random.choice(items)
+
